25b10cda100b75a516d20e689114ae4b579045dd
fifiteen82726 <fifiteen82726@gmail.com>
2016-06-14 14:38:05 +0800

    add  add_swagger_documentation

--- app/api/twitter/api.rb
+++ app/api/twitter/api.rb
@@ -1,19 +1,17 @@
-require 'grape'
+#app/api/twitter/api/api.rb
 
+require 'grape'
+require 'grape-swagger'
 
 module Twitter
   class API < Grape::API
     version 'v1', using: :header, vendor: 'twitter'
     format :json
     prefix :api
+    add_swagger_documentation
 
     resource :statuses do
       desc 'Return a public timeline.'
-      get :hahaha do
-        # Status.limit(20)
-        {:say => 'hahaha'}
-      end
-
 
       get :public_timeline do
         Status.limit(20)
